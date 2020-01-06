56c20c44ee9dcef1e304a90f8d77ae0ad48381bf
adelie.thirion@gmail.com <adelie.thirion@gmail.com>
2020-01-06 12:46:58 +0100

    Simple 2way data biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'navette';
+export class AppComponent implements OnInit {
+  public appTitle = 'navette';
+  public options: string[];
+  public option: string; 
+
+  public constructor() {
+    this.options = new Array<string>() ;
+
+  }
+
+  public ngOnInit(): void{
+    this.options.push('Accueil');
+    this.options.push('Navette');
+    this.options.push('Mon compte');
+    this.options.push('Contact');
+  }
+
+  public addOption(): void {
+    if (!this.checkOption()) {
+      this.options.push(this.option);
+    }         
+  }
+
+  public checkOption(): boolean {
+    return !(this.option && this.option.trim().length > 0);
+  }
+
 }
